a9f7dd8b566418d9c44a5fb3f32a8e4ee9c75563
Andrey <example@gmail.com>
2020-11-08 09:47:45 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-let number = 5;
-const leftBorderWidth = 1;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-number = 10;
-console.log(number);
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
+
+personalMovieDB[a] = b; 
+personalMovieDB[c] = d;  
+
+console.log(personalMovieDB);
